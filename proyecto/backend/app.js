d3ed4ce6d50b3d86b7f1c92aef08dcae07c5d50f
davidols12 <116851084+davidols12@users.noreply.github.com>
2024-04-25 10:54:12 -0700

    Proyecto - Conexion MySQL

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -6,9 +6,38 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+//Conexion a MySQL
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//Nos conectamos a MySQL
+conexion.connect(function (error) {
+    if (error) {
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos');
+    }
+});
+
 //rutas de acceso
 app.get("/",function(req,res){
-    res.send("Ruta de inicios");
+    res.send("Ruta de inicio");
+});
+
+//seleccionamos todos los clientes
+app.get(('/api/clientes'),(req,res)=>{
+    conexion.query('SELECT * FROM clientes', (error,filas)=>{
+        if (error) {
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
 });
 
 //Encender servidor
